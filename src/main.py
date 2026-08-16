030e86854c0aecc1ca31aa1396e6bb56e757cec3
Jacqueline Andrade <39661043+jacquelineandrade@users.noreply.github.com>
2026-08-16 20:35:55 -0300

    Criando dataframe

--- src/main.py
+++ src/main.py
@@ -2,3 +2,6 @@ import pandas as pd
 import requests
 
 r = requests.get('https://dadosabertos.camara.leg.br/api/v2/deputados?ordem=ASC&ordenarPor=nome')
+
+if r.status_code == 200:
+    df = pd.DataFrame(r.json()['dados'])
