d763ea873fd810646cc79072e891b55f97dae247
Jacqueline Andrade <39661043+jacquelineandrade@users.noreply.github.com>
2026-09-11 01:38:36 -0300

    Criação do app

---
+++ app.py
@@ -0,0 +1,40 @@
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
+import requests
+
+r = requests.get('https://dadosabertos.camara.leg.br/api/v2/deputados?ordem=ASC&ordenarPor=nome')
+df = pd.DataFrame(r.json()['dados'])
+
+app = Dash()
+
+app.layout = [
+    html.H1(children='Deputados Federais - Visão Geral', style={'textAlign':'center', 'font-family':'Arial'}),
+    dcc.Graph(
+        figure=px.histogram(
+            df,
+            title='Qtd de Deputados por UF',
+            x='siglaUf',
+            y='id',
+            histfunc='count',
+            category_orders={'siglaUf': df['siglaUf'].value_counts().index},
+            text_auto=True,
+            labels={'siglaUf':'UF'}
+        )
+    ),
+    dcc.Graph(
+        figure=px.histogram(
+            df,
+            title='Qtd de Deputados por Partido',
+            x='siglaPartido',
+            y='id',
+            histfunc='count',
+            category_orders={'siglaPartido': df['siglaPartido'].value_counts().index},
+            text_auto=True,
+            labels={'siglaPartido':'Partido'}
+        )
+    )
+]
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=False, port=8050)
